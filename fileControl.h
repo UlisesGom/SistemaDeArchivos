1dd3d40030f1998b15a634d0dc1e86a08b9df48c
ie694647@iteso.mx <Usuario@F10119.ds.iteso.mx>
2018-09-05 18:11:34 -0500

    Hola

--- fileControl.h
+++ fileControl.h
@@ -5,8 +5,7 @@ int setninode(int num, char *filename,unsigned short atribs, int uid, int gid);
 int searchinode(char *filename); 
 int removeinode(int numinode);
 
-//Gestion de archivos 
-int vdopen(char *filename,unsigned short mode); 
+//Gestion de archivos  
 int vdcreat(char *filename,unsigned short perms); 
 int vdunlink(char *filename); 
 int vdseek(int fd, int offset, int whence); 
